b2bc0bfa385087f7fd9f951108c929d7af66c9e4
오유택 <oyutaeg@oyutaeg-ui-MacBookPro.local>
2022-01-07 22:36:33 +0900

    uteak-code

--- edaproj/edaproj/settings.py
+++ edaproj/edaproj/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "'django-insecure-@tr2i%gc=9lra!vx_q-19bw7dtxghbzp++bj1vd79k2s*%l#ra'")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-@tr2i%gc=9lra!vx_q-19bw7dtxghbzp++bj1vd79k2s*%l#ra')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'edaproj.urls'
